0123f18f862aecf465fd30a21e9decbe589a3bec
Skylar <skylar.b.payne@gmail.com>
2013-08-04 23:35:15 -0700

    Issue 4. Can now check keystates from Lua.

--- LuaBindings.cpp
+++ LuaBindings.cpp
@@ -15,14 +15,26 @@ extern "C"
 /**
  * @brief exit Lets lua emit an exit message
  * @param L the lua thread to perform this function on
- * @return the exit status
+ * @return 0
  */
 static int Exit(lua_State* L)
 {
     ExitMessage msg;
     msg.ExitStatus = lua_tonumber(L, 1);
     Emit<ExitMessage>(msg);
-    return msg.ExitStatus;
+    return 0;
+}
+
+/**
+ * @brief GetKeyState gets the key state of a certain key
+ * @param L the thread to perform this function on
+ * @return 1
+ */
+static int isKeyPressed(lua_State* L)
+{
+    sf::Keyboard::Key key = static_cast<sf::Keyboard::Key>(lua_tointeger(L, 1));
+    lua_pushboolean(L, sf::Keyboard::isKeyPressed(key));
+    return 1;
 }
 
 /**
@@ -66,4 +78,47 @@ void SetBindings(lua_State *L)
 
     lua_pushcfunction(L, Exit);
     lua_setglobal(L, "exit");
+
+    lua_pushcfunction(L, isKeyPressed);
+    lua_setglobal(L, "is_key_pressed");
+
+    lua_pushcfunction(L, MoveEntity);
+    lua_setglobal(L, "move_entity");
+
+    lua_pushcfunction(L, PushEntity);
+    lua_setglobal(L, "push_entity");
+
+    lua_newtable(L);
+    lua_pushliteral(L, "W");
+    lua_pushnumber(L, sf::Keyboard::W);
+    lua_settable(L, -3);
+
+    lua_pushliteral(L, "S");
+    lua_pushnumber(L, sf::Keyboard::S);
+    lua_settable(L, -3);
+
+    lua_pushliteral(L, "A");
+    lua_pushnumber(L, sf::Keyboard::A);
+    lua_settable(L, -3);
+
+    lua_pushliteral(L, "D");
+    lua_pushnumber(L, sf::Keyboard::D);
+    lua_settable(L, -3);
+
+    lua_pushliteral(L, "Up");
+    lua_pushnumber(L, sf::Keyboard::Up);
+    lua_settable(L, -3);
+
+    lua_pushliteral(L, "Down");
+    lua_pushnumber(L, sf::Keyboard::Down);
+    lua_settable(L, -3);
+
+    lua_pushliteral(L, "Left");
+    lua_pushnumber(L, sf::Keyboard::Left);
+    lua_settable(L, -3);
+
+    lua_pushliteral(L, "Right");
+    lua_pushnumber(L, sf::Keyboard::Right);
+    lua_settable(L, -3);
+    lua_setglobal(L, "Keys");
 }
